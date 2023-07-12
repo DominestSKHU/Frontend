b0efecc7a0113755e91447a17660f5fc6d742ba1
LeeHueeng <zzxx373014@gmail.com>
2023-07-12 11:22:15 +0900

    studentdate 편집

--- dominest/src/components/studentdate.tsx
+++ dominest/src/components/studentdate.tsx
@@ -1,40 +1,73 @@
 import { css } from "@emotion/react";
-import React, { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 import "../app/globals.css";
 /** @jsxImportSource @emotion/react */
 
-export default function studentdate() {
+export default function StudentDate() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    // 데이터 조회 요청을 useEffect 내에서 수행합니다.
+    axios
+      .get("http://domidomi.duckdns.org/residents")
+      .then((response) => {
+        setData(response.data);
+        console.log(response.data); // 조회된 데이터를 상태로 설정합니다.
+      })
+      .catch((error) => {
+        console.error("데이터 조회 중 오류 발생:", error);
+      });
+  }, []);
+
   const renderTable = () => {
     const rows = [];
 
-    for (let i = 0; i < 130; i++) {
-      const cells = [];
+    if (data.length > 0) {
+      const headerRow = Object.keys(data[0]).map((key) => (
+        <th
+          key={key}
+          css={css`
+            padding: 5px 1px;
+            min-width: 120px;
+            white-space: nowrap;
+            border: 1px solid black;
+          `}
+        >
+          {key}
+        </th>
+      ));
 
-      for (let j = 0; j < 25; j++) {
-        cells.push(
-          <td
-            key={j}
-            css={css`
-              padding: 5px 1px;
-              min-width: 120px;
-              white-space: nowrap;
-              border: 1px solid black;
-            `}
-          >
-            {"안녕하세요"}
-          </td>
+      rows.push(<tr key="header">{headerRow}</tr>);
+
+      for (let i = 0; i < data.length; i++) {
+        const rowData = (Object.values(data[i]) as string[]).map(
+          (value, index) => (
+            <td
+              key={index}
+              css={css`
+                padding: 5px 1px;
+                min-width: 120px;
+                white-space: nowrap;
+                border: 1px solid black;
+              `}
+            >
+              {value}
+            </td>
+          )
         );
-      }
 
-      rows.push(<tr key={i}>{cells}</tr>);
+        rows.push(<tr key={i}>{rowData}</tr>);
+      }
     }
+
     return (
       <table
         css={css`
           border-collapse: collapse;
           margin: 10px;
-          border-rounder: 5px;
+          border-radius: 5px;
         `}
       >
         <tbody>{rows}</tbody>
