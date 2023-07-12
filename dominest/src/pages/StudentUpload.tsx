487166ba177f08f38c7d5744c7b7d36a89385b28
LeeHueeng <zzxx373014@gmail.com>
2023-07-12 10:38:24 +0900

    업로드 기능 구현

--- dominest/src/pages/StudentUpload.tsx
+++ dominest/src/pages/StudentUpload.tsx
@@ -18,25 +18,15 @@ export default function studentupload() {
       const formData = new FormData();
       formData.append("file", selectedFile);
 
-      const curlCommand = `curl -X POST -F "file=@${selectedFile.name}" http://domidomi.duckdns.org/residents/upload-excel`;
-
-      sendRequestToServer(curlCommand);
+      axios
+        .post("http://domidomi.duckdns.org/residents/upload-excel", formData)
+        .then((response) => {
+          console.log("요청이 성공적으로 전송되었습니다.");
+        })
+        .catch((error) => {
+          console.error("요청 전송 중 오류가 발생했습니다:", error);
+        });
     }
-  };
-
-  const sendRequestToServer = (command) => {
-    axios
-      .post("http://domidomi.duckdns.org/residents/upload-excel", {
-        command: command,
-      })
-      .then((response) => {
-        console.log("요청이 성공적으로 전송되었습니다.");
-        // 추가로 처리할 작업을 수행할 수 있습니다.
-      })
-      .catch((error) => {
-        console.error("요청 전송 중 오류가 발생했습니다:", error);
-        // 오류 처리를 수행할 수 있습니다.
-      });
   };
 
   return (
