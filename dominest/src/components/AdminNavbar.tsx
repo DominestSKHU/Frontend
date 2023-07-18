782e2cfb1a354a59dc713f6d6cc67a5dee7be454
seulseul_0123 <104067367+yeaseul7@users.noreply.github.com>
2023-07-18 19:00:34 +0900

    Design: 관리자 Navbar생성 및 css

---
+++ dominest/src/components/AdminNavbar.tsx
@@ -0,0 +1,168 @@
+import React from "react";
+import styled from "@emotion/styled";
+import Link from "next/link";
+import { css } from "@emotion/react";
+import { HiOutlineUserCircle } from "react-icons/hi";
+/** @jsxImportSource @emotion/react */
+
+const ListStyle = styled.li`
+  margin-left: 10%;
+  width: 7rem;
+  & > ul {
+    display: inline-block;
+    list-style: none;
+    width: max-content;
+    position: absolute;
+    top: 32px;
+    & > p {
+      margin: 5px;
+    }
+    &:hover {
+      & > li {
+        display: block;
+      }
+    }
+    & > li {
+      background-color: rgb(202, 202, 202);
+      margin-top: 5px;
+      padding: 5px 10px;
+      width: initial;
+      border-radius: 5px;
+      display: none;
+      &:hover {
+        background-color: rgb(189, 189, 189);
+        box-shadow: 0px 3px 10px rgba(0, 0, 0, 0.147);
+      }
+      & > a {
+        text-decoration: none;
+        color: black;
+      }
+    }
+  }
+`;
+const UserStyle = styled.p`
+  margin-right: 5px;
+  font-weight: bold;
+  width: 3em;
+`;
+const User = styled.div`
+  display: flex;
+  align-items: center;
+  width: fit-content;
+  background-color: #dcdcdc;
+  border-radius: 20px;
+  padding: 0px 10px;
+  box-shadow: 0px 3px 10px rgba(0, 0, 0, 0.147);
+  width: fit-content;
+`;
+const LogoutStyle = styled.div`
+  margin-right: 10%;
+  `
+export default function Navber() {
+  const [name, setName] = React.useState("이용자");
+  const [role, setRole] = React.useState("근로생");
+  return (
+    <div>
+      <nav
+        css={css`
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          width: 100vw;
+          height: 10vh;
+          background-color: rgb(173, 173, 173);
+        `}
+      >
+        <div
+          css={css`
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            width: fit-content;
+            padding-left: 10%;
+          `}
+        >
+          <div
+            className="logo"
+            css={css`
+              font-size: 1.2em;
+              font-weight: bold;
+              margin-right: 10%;
+            `}
+          >
+            Dominest
+          </div>
+          <ul
+            css={css`
+              display: flex;
+              justify-content: space-between;
+              align-items: center;
+              background-color: var(--background-color);
+              padding: 8px 12px;
+              list-style: none;
+            `}
+          >
+            <ListStyle>
+              <ul>
+                <p>즐겨찾기</p>
+                <li>
+                  <Link href="/login">땡땡이</Link>
+                </li>
+              </ul>
+            </ListStyle>
+            <ListStyle>
+              <ul>
+                <p>관리자 업무</p>
+                <li>
+                  <Link href="/login">권한 부여</Link>
+                </li>
+                <li>
+                  <Link href="/login">땡땡이</Link>
+                </li>
+                <li>
+                  <Link href="/login">빵빵이</Link>
+                </li>
+              </ul>
+            </ListStyle>
+            <ListStyle>
+              <ul>
+                <p>근로생 업무</p>
+                <li>
+                  <Link href="/login">땡땡이</Link>
+                </li>
+                <li>
+                  <Link href="/login">빵빵이</Link>
+                </li>
+                <li>
+                  <Link href="/login">옥지얌</Link>
+                </li>
+              </ul>
+            </ListStyle>
+          </ul>
+        </div>
+        <LogoutStyle>
+          <User className="login">
+            <HiOutlineUserCircle
+              size={25}
+              css={css`
+                margin-right: 5px;
+              `}
+            />
+            {role === "근로생" ? (
+              <UserStyle>근로생</UserStyle>
+            ) : (
+              <UserStyle>관리자</UserStyle>
+            )}
+            <UserStyle
+              css={css`
+                color: green;
+              `}
+            >
+              {name}
+            </UserStyle>
+          </User>
+        </LogoutStyle>
+      </nav>
+    </div>
+  );
+}
--- dominest/src/pages/login.tsx
+++ dominest/src/pages/login.tsx
@@ -5,45 +5,49 @@ import { BiUser } from "react-icons/bi";
 import { RiLockPasswordLine } from "react-icons/ri";
 import { css } from "@emotion/react";
 import "../app/globals.css";
+import AdminNavbar from "../components/AdminNavbar";
 /** @jsxImportSource @emotion/react */
 
 export default function Login() {
   return (
-    <div
-      css={css`
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        height: 100vh;
-        background-color: rgb(255, 255, 255);
-      `}
-    >
-      <Form action="" className="form_main">
-        <p className="heading">Login</p>
-        <div className="inputContainer">
-          <BiUser />
-          <input
-            type="text"
-            className="inputField"
-            id="username"
-            placeholder="아이디를 입력해주세요"
-          />
-        </div>
+    <div>
+      <AdminNavbar />
+      <div
+        css={css`
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          height: 90vh;
+          background-color: rgb(255, 255, 255);
+        `}
+      >
+        <Form action="" className="form_main">
+          <p className="heading">Login</p>
+          <div className="inputContainer">
+            <BiUser />
+            <input
+              type="text"
+              className="inputField"
+              id="username"
+              placeholder="아이디를 입력해주세요"
+            />
+          </div>
 
-        <div className="inputContainer">
-          <RiLockPasswordLine />
-          <input
-            type="password"
-            className="inputField"
-            id="password"
-            placeholder="비밀번호를 입력해주세요"
-          />
-        </div>
+          <div className="inputContainer">
+            <RiLockPasswordLine />
+            <input
+              type="password"
+              className="inputField"
+              id="password"
+              placeholder="비밀번호를 입력해주세요"
+            />
+          </div>
 
-        <button id="button">Submit</button>
-        <Link href="/">아이디를 잃어버리셨나요?</Link>
-        <Link href="/">비밀번호를 잃어버리셨나요?</Link>
-      </Form>
+          <button id="button">Submit</button>
+          <Link href="/">아이디를 잃어버리셨나요?</Link>
+          <Link href="/">비밀번호를 잃어버리셨나요?</Link>
+        </Form>
+      </div>
     </div>
   );
 }
