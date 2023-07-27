a1aacabd9ecbf43ebfad6d110171bc20de709a7e
LeeHueeng <zzxx373014@gmail.com>
2023-07-27 14:16:26 +0900

    pdf list 컴포넌트 제작

--- dominest/src/pages/pdfupload.tsx
+++ dominest/src/pages/pdfupload.tsx
@@ -1,33 +1,36 @@
 import React, { useState } from "react";
 import { css } from "@emotion/react";
 import styled from "@emotion/styled";
-
+import PdfList from "dominest/src/components/PdfList.tsx";
+/** @jsxImportSource @emotion/react */
 export default function pdfupload() {
   return (
     <UploadForm>
+      <PdfList></PdfList>
+      <div className="time">
+        <select>
+          <option value="">연도 선택</option>
+          <option value="S2023">2023년</option>
+          <option value="S2024">2024년</option>
+          <option value="S2025">2025년</option>
+          <option value="S2026">2026년</option>
+          <option value="S2027">2027년</option>
+          <option value="S2028">2028년</option>
+          <option value="S2029">2029년</option>
+          <option value="S2030">2030년</option>
+        </select>
+        <select>
+          <option value="">차수 선택</option>
+          <option value="_1">1학기</option>
+          <option value="_SUMMER">여름학기</option>
+          <option value="_2">2학기</option>
+          <option value="_WINTER">겨울학기</option>
+        </select>
+        <button>조회</button>
+      </div>
       <div className="upload">
         <label htmlFor="file">파일 업로드</label>
         <input type="file" id="file" accept=".zip" />
-        <div className="time">
-          <select>
-            <option value="">연도 선택</option>
-            <option value="S2023">2023년</option>
-            <option value="S2024">2024년</option>
-            <option value="S2025">2025년</option>
-            <option value="S2026">2026년</option>
-            <option value="S2027">2027년</option>
-            <option value="S2028">2028년</option>
-            <option value="S2029">2029년</option>
-            <option value="S2030">2030년</option>
-          </select>
-          <select>
-            <option value="">차수 선택</option>
-            <option value="_1">1학기</option>
-            <option value="_SUMMER">여름학기</option>
-            <option value="_2">2학기</option>
-            <option value="_WINTER">겨울학기</option>
-          </select>
-        </div>
         <button>업로드 </button>
       </div>
     </UploadForm>
@@ -41,6 +44,7 @@ const UploadForm = styled.div`
     select {
       margin-left: 10px;
       border-radius: 10px;
+      height: 40px;
     }
   }
   .upload {
@@ -80,6 +84,7 @@ const UploadForm = styled.div`
     margin-left: 20px;
     border-radius: 10px;
     width: 80px;
+
     &:hover {
       background: rgb(77, 77, 77);
       color: #fff;
