94b46ce9ac61fda3d824d547512e58c0a196b838
seulseul_0123 <104067367+yeaseul7@users.noreply.github.com>
2023-07-28 16:17:41 +0900

    Design: 카테고리 추가 UI/IX 2차
    미완성

--- dominest/src/pages/categoryManage.tsx
+++ dominest/src/pages/categoryManage.tsx
@@ -5,6 +5,7 @@ import styled from "@emotion/styled";
 import { useRouter } from "next/navigation";
 import { BsList } from "react-icons/bs";
 import { AiOutlinePlus } from "react-icons/ai";
+import { RxDividerVertical } from "react-icons/rx";
 import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 import { useEffect } from "react";
 /** @jsxImportSource @emotion/react */
@@ -39,10 +40,12 @@ const CategoryBox = styled.div`
   border: 1px solid #cdcdcd;
   border-radius: 20px;
   padding: 2%;
-  height: 82.5%;
+  height: 88%;
   & > .boxComment {
     display: flex;
     justify-content: space-between;
+    align-items: center;
+    height: 6%;
     & > .control {
       width: fit-content;
       display: flex;
@@ -68,10 +71,10 @@ const CategoryBox = styled.div`
 `;
 
 const Category = styled.div`
-  background-color: aliceblue;
-  height: 85%;
+  background-color: #f3f3f3;
+  height: 90%;
   margin: 1% 0%;
-  padding: 2%;
+  padding: 10px;
 `;
 
 const CaregorySub = styled.div`
@@ -92,13 +95,73 @@ const CategoryPlus = styled.button`
   background-color: white;
 `;
 
+const SaveChange = styled.button`
+  background-color: #ffffff;
+  padding: 1%;
+  border: 1px solid #aeaeae;
+  border-radius: 10px;
+  margin: 0.6% 2%;
+  font-size: large;
+`;
+
+const CategoryMoveBox = styled.div`
+  height: 82%;
+  border: 1px solid lightgrey;
+  overflow: scroll;
+  & > .moveBtn {
+    background-color: white;
+  }
+`;
+const explanInput = css`
+  width: 20em;
+  font-size: 1.2em;
+  outline: none;
+  border: none;
+  background-color: aliceblue;
+`;
+
 const CategoryPlusBox = () => {
   return (
-    <div>
-      <button>
-        <MdOutlineKeyboardArrowRight />
+    <div
+      css={css`
+        display: flex;
+        border-bottom: 1px solid lightgrey;
+        align-items: center;
+        height: 14.1%;
+      `}
+    >
+      <button
+        className="moveBtn"
+        css={css`
+          background-color: #e3e3e3;
+          border: none;
+          height: 100%;
+        `}
+      >
+        <BsList size={25} />
       </button>
-      <div>방역 및 호실 점검</div>
+      <div
+        css={css`
+          font-size: 1.2rem;
+          font-weight: bold;
+          display: flex;
+          align-items: center;
+          width: 96%;
+          height: 100%;
+          justify-content: space-evenly;
+        `}
+      >
+        <span
+          css={css`
+            width: 20em;
+          `}
+        >
+          방역 및 호실 점검
+        </span>
+
+        <RxDividerVertical size={40} color="#d4d4d4" />
+        <input css={explanInput}></input>
+      </div>
     </div>
   );
 };
@@ -147,17 +210,37 @@ const categoryManage = () => {
             </div>
             <Category className="categoryBox">
               <CaregorySub>
-                <BsList size={20} />
-                <span
+                <div
                   css={css`
-                    font-size: 1.5rem;
-                    margin-left: 1%;
+                    display: flex;
+                    align-items: center;
+                    margin: 1% 0%;
                   `}
                 >
-                  분류
-                </span>
-                <hr></hr>
-                <CategoryPlusBox></CategoryPlusBox>
+                  <div>
+                    <BsList size={20} />
+                  </div>
+                  <span
+                    css={css`
+                      font-size: 1.5rem;
+                      margin-left: 1%;
+                    `}
+                  >
+                    분류
+                  </span>
+                </div>
+                <CategoryMoveBox>
+                  <CategoryPlusBox></CategoryPlusBox>
+                  <CategoryPlusBox></CategoryPlusBox>
+                  <CategoryPlusBox></CategoryPlusBox>
+                  <CategoryPlusBox></CategoryPlusBox>
+                  <CategoryPlusBox></CategoryPlusBox>
+                  <CategoryPlusBox></CategoryPlusBox>
+                  <CategoryPlusBox></CategoryPlusBox>
+                  <CategoryPlusBox></CategoryPlusBox>
+                  <CategoryPlusBox></CategoryPlusBox>
+                  <CategoryPlusBox></CategoryPlusBox>
+                </CategoryMoveBox>
                 <CategoryPlus>
                   <div
                     className="leftPlusBlock"
@@ -178,8 +261,14 @@ const categoryManage = () => {
                   <span className="rightPlus">22/500</span>
                 </CategoryPlus>
               </CaregorySub>
-              <div className="saveChange">
-                <button>변경사항 저장</button>
+              <div
+                className="saveChange"
+                css={css`
+                  display: flex;
+                  justify-content: flex-end;
+                `}
+              >
+                <SaveChange>변경사항 저장</SaveChange>
               </div>
             </Category>
           </CategoryBox>
