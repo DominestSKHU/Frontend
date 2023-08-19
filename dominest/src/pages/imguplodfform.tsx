d53f8090b1f81fc4a73fc55a21e4860a1aeb71f7
LeeHueeng <zzxx373014@gmail.com>
2023-08-19 14:21:19 +0900

    img 업로드 css 수정

--- dominest/src/pages/imguplodfform.tsx
+++ dominest/src/pages/imguplodfform.tsx
@@ -4,16 +4,7 @@ import { css } from "@emotion/react";
 import styled from "@emotion/styled";
 import Navbar from "@/components/AdminNavbar";
 import Image from "next/image";
-
-const ImageInputStyle = css`
-  .upload-box {
-    width: 500px;
-    height: 300px;
-    border: 1px solid gray;
-    box-shadow: 2px 3px 9px hsl(0, 0%, 47%);
-    padding: 10px;
-  }
-`;
+import cloudImage from "style/cloude.png";
 
 const Inputt = styled.div`
   input {
@@ -21,14 +12,80 @@ const Inputt = styled.div`
   }
 `;
 
-const ImageInputContainer = styled.div`
-  ${ImageInputStyle}
-`;
-
 const PreviewContainer = styled.div`
   display: flex;
   overflow-x: auto;
   gap: 10px;
+`;
+const TitleInput = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 10px;
+  input {
+    margin-left: 10px;
+    width: 80%;
+    height: 30px;
+    border: 1px solid gray;
+    border-radius: 10px;
+  }
+`;
+
+const ImageInput = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  margin-top: 30px;
+  .file-preview {
+    width: 100px;
+    height: 100px;
+    border: 1px solid gray;
+    border-radius: 10px;
+    overflow: hidden;
+  }
+  .file-preview img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+  .file-preview + .file-preview {
+    margin-left: 10px;
+  }
+  .file-preview:first-of-type {
+    margin-left: 0;
+  }
+  .file-preview:last-of-type {
+    margin-right: 0;
+  }
+
+
+  .upload-box {
+    width: 80%;
+    height: 300px;
+    border: 1px solid gray;
+    box-shadow: 2px 3px 9px hsl(0, 0%, 47%);
+    padding: 10px;
+    border-radius: 10px;
+    cursor: pointer;
+    &:hover {
+      background: rgb(77, 77, 77);
+      color: #fff;
+  }
+  .noneimg {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    img {
+      width: 100px;
+      height: 100px;
+    }
+    p {
+      margin-top: 10px;
+    }
+  }
+
 `;
 
 export default function ImageUploadForm() {
@@ -53,48 +110,59 @@ export default function ImageUploadForm() {
   return (
     <div>
       <Navbar pageTitle="이미지 업로드 Form" />
-      <ImageInputContainer>
-        <label>제목</label> <input type="text" />
+      <div>
         <section id="ex9">
-          <h1>파일 업로드: DND & Trigger</h1>
-
-          <div
-            className="upload-box"
-            onDrop={handleDrop}
-            onClick={handleUploadTextClick}
-            onDragOver={(e) => e.preventDefault()}
-          >
-            <Inputt>
-              <input
-                ref={fileInputRef}
-                className="btn-file d-none"
-                type="file"
-                onChange={handleFileChange}
-                multiple
-              />
-              {selectedFiles.length > 0 ? (
-                <div>
-                  <p>미리보기:</p>
-                  <PreviewContainer>
-                    {selectedFiles.map((file, index) => (
-                      <div key={index} className="file-preview">
-                        <Image
-                          src={URL.createObjectURL(file)}
-                          alt={`미리보기 ${index + 1}`}
-                          width={100}
-                          height={100}
-                        />
-                      </div>
-                    ))}
-                  </PreviewContainer>
-                </div>
-              ) : (
-                <p>여기에 파일을 드래그 앤 드롭하세요.</p>
-              )}
-            </Inputt>
-          </div>
+          <h1>사진</h1>
+          <TitleInput>
+            <label>제목</label> <input type="text" />
+          </TitleInput>
+          <ImageInput>
+            <div
+              className="upload-box"
+              onDrop={handleDrop}
+              onClick={handleUploadTextClick}
+              onDragOver={(e) => e.preventDefault()}
+            >
+              <Inputt>
+                <input
+                  ref={fileInputRef}
+                  className="btn-file d-none"
+                  type="file"
+                  onChange={handleFileChange}
+                  multiple
+                />
+                {selectedFiles.length > 0 ? (
+                  <div>
+                    <p>미리보기:</p>
+                    <div>
+                      {selectedFiles.map((file, index) => (
+                        <div key={index} className="file-preview">
+                          <Image
+                            src={URL.createObjectURL(file)}
+                            alt={`미리보기 ${index + 1}`}
+                            width={100}
+                            height={100}
+                          />
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                ) : (
+                  <div className="noneimg">
+                    <img
+                      src={cloudImage}
+                      alt="클라우드 이미지"
+                      width={1000}
+                      height={1000}
+                    />
+                    <p>사진을 끌어서 넣거나 클릭해주세요</p>
+                  </div>
+                )}
+              </Inputt>
+            </div>
+          </ImageInput>
         </section>
-      </ImageInputContainer>
+      </div>
     </div>
   );
 }
