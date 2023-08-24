0bcd428837319e25250007ec2ad599b58ad1de72
LeeHueeng <zzxx373014@gmail.com>
2023-08-25 00:04:30 +0900

    파일병합작업

---
+++ dominest/src/style/DivStyle.tsx
@@ -0,0 +1,54 @@
+/** @jsxImportSource @emotion/react */
+
+import styled from "@emotion/styled";
+
+export const TopLevelDiv = styled.div`
+  background-color: #d7d7d7;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 1%;
+  width: 86vw;
+  height: 86%;
+  border-radius: 20px;
+`;
+
+export const TwiceLevelDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 95%;
+  height: 100%;
+  margin: 2% 0.5%;
+  padding: 2%;
+  border-radius: 20px;
+  background-color: white;
+`;
+
+export const ListUl = styled.ul`
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  overflow: scroll;
+`;
+
+export const ListLi = styled.li`
+  width: 94%;
+  height: 3rem;
+  list-style: none;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 2rem 0 1rem;
+  font-size: 1.3rem;
+  border: 1px solid #b2b2b2;
+  border-radius: 10px;
+  margin: 1%;
+  &:hover,
+  &:focus {
+    background-color: #dedede;
+    transform: scale(1.01);
+    transition: all 0.3s ease-in-out;
+  }
+`;
