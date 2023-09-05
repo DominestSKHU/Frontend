fe170dcf2265c1f74b0ea77afaaad5a2c8274c09
seulseul_0123 <104067367+yeaseul7@users.noreply.github.com>
2023-09-05 23:58:05 +0900

    병합과정2

--- dominest 복사본/src/utils/AddUtil.tsx
+++
@@ -1,9 +0,0 @@
-export const handleInputChange = (e, field) => {
-    let value = e.target.value;
-  
-    setResidentId((prevResident) => ({
-      ...prevResident,
-      [field]: value,
-    }));
-    console.log(value);
-  };
