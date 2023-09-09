aff7f3092c2d4f8b3035e73b859b649c2855a26e
LeeHueeng <zzxx373014@gmail.com>
2023-09-10 00:41:33 +0900

    파일 다운로드

--- dominest/src/utils/useAuth/loginUtil.tsx
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-//로그인
-export const loginUtil = (email: string, password: string) =>
-  axios.post(`${process.env.NEXT_PUBLIC_API_URL}/user/login`, {
-    email,
-    password,
-  });
