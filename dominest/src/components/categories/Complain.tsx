ee56ae4fe82ace2d5a939c45a6ed9bf7883d8005
LeeHueeng <zzxx373014@gmail.com>
2023-09-10 02:26:27 +0900

    검색 설정 select 수정 만약 민원조회시 검색창 방조회시 호실 select 처리상태 조회시 처리중, 처리완료 select 가 보이게 수정

--- dominest/src/components/categories/Complain.tsx
+++ dominest/src/components/categories/Complain.tsx
@@ -7,12 +7,29 @@ import { Button, Containerright } from "@/style/InputStyle";
 import "../../app/globals.css";
 import { Container, Table, ButtonContainer } from "@/style/border";
 
+interface Post {
+  id: number;
+  title: string;
+  roomNo: string;
+  createTime: string;
+  type: string;
+  lastModifiedBy: string;
+  lastModifiedTime: string;
+  complaintCause: string;
+  complaintResolution: string;
+  processState: string;
+  createdBy: string;
+  date: string;
+}
+
 export default function Complaints(props: any) {
   const [posts, setPosts] = useState<Post[]>([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
   const [categoryName, setCategoryName] = useState("");
-
+  const [status, setStatus] = useState("민원 조회");
+  const [serch, setSerch] = useState("");
+  const [statusresult, setStatusresult] = useState("처리중");
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -30,6 +47,16 @@ export default function Complaints(props: any) {
     fetchData();
   }, [currentPage]);
 
+  const onChangeStatus = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setStatus(e.target.value);
+  };
+  const onChangeSerch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSerch(e.target.value);
+  };
+  const onChangeStatusresult = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setStatusresult(e.target.value);
+  };
+
   return (
     <div>
       <ComponenComplaints>
@@ -38,14 +65,26 @@ export default function Complaints(props: any) {
         </div>
         <div>
           <div>
-            <select>
-              <option value="전체 조회">전체 조회</option>
-              <option value="호실 조회">호실 조회</option>
+            <select onChange={onChangeStatus}>
               <option value="민원 조회">민원 조회</option>
+              <option value="호실 조회">호실 조회</option>
               <option value="처리 조회">처리 조회</option>
             </select>
-            <RoomSelector />
-            <input type="text" placeholder="검색어를 입력하세요" />
+            {status === "민원 조회" ? (
+              <input
+                type="text"
+                placeholder="검색어를 입력하세요"
+                onChange={onChangeSerch}
+              />
+            ) : status === "호실 조회" ? (
+              <RoomSelector />
+            ) : (
+              <select onChange={onChangeStatusresult}>
+                <option value="처리중">처리중</option>
+                <option value="처리완료">처리완료</option>
+              </select>
+            )}
+
             <button>조회</button>
             <button>추가</button>
           </div>
@@ -73,7 +112,15 @@ export default function Complaints(props: any) {
                   <td>{post.roomNo}</td>
                   <td>{post.complaintCause}</td>
                   <td>{post.complaintResolution}</td>
-                  <td>{post.processState}</td>
+                  <td
+                    style={{
+                      color: post.processState === "처리중" ? "red" : "black",
+                      fontWeight:
+                        post.processState === "처리중" ? "bold" : "normal",
+                    }}
+                  >
+                    {post.processState}
+                  </td>
                   <td>{post.createdBy}</td>
                   <td>{post.date}</td>
                 </tr>
