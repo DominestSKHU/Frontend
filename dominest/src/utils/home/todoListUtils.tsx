4343fe59ac6d52c95acff6ff8a80bf3f5f451402
seulseul_0123 <cutie32769@gmail.com>
2023-09-15 14:29:49 +0900

    feat: checkYn 부분 제외하고 todoList api 연동

---
+++ dominest/src/utils/home/todoListUtils.tsx
@@ -0,0 +1,44 @@
+import axios from "axios";
+
+export const todoListGet = (token: string): Promise<any> => {
+  return axios.get("https://domidomi.duckdns.org/todo/list", {
+    headers: {
+      "Content-Type": `application/json;charset=UTF-8`,
+      Accept: "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+};
+
+export const postTodoList = (
+  token: string,
+  task: string,
+  receiveRequest: string
+): Promise<any> => {
+  const data = {
+    task: task,
+    receiveRequest: receiveRequest,
+  };
+
+  return axios.post("https://domidomi.duckdns.org/todo/save", data, {
+    headers: {
+      "Content-Type": `application/json;charset=UTF-8`,
+      Accept: "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+};
+
+export const updateTodoList = (
+    token: string,
+    id: number,
+    checkYn: boolean
+  ): Promise<any> => {
+    return axios.put(`https://domidomi.duckdns.org/todo/${id}/check?checkYn=${checkYn}`, {
+      headers: {
+        "Content-Type": `application/json;charset=UTF-8`,
+        Accept: "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+  };
